5ae35d4c019de04502776c78b4b6d453b82beb90
殊怀丶 <lvzhihe_123@qq.com>
2021-10-09 12:56:27 +0800

    fix(main.ts): change baseURL

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import router from './router' // 路由引入
 import naive from './naive' // naive-ui 组件局部引入
 import axios from 'axios'
 const app = createApp(App)
-axios.defaults.baseURL = "http://192.168.1.101:8000/api/v1/";
+axios.defaults.baseURL = "http://s3.s100.vip:19231/api/v1/";
 app.use(naive)
 app.use(router)
 app.mount('#app')
