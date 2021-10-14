0fc85bd07012771db0d7b6b37b69a7e648642f92
殊怀丶 <lvzhihe_123@qq.com>
2021-10-14 23:58:02 +0800

    added users/me

--- src/views/home.vue
+++ src/views/home.vue
@@ -15,5 +15,34 @@
 <script lang="ts" setup>
 import { PlayerCard } from '../components'
 import { storage } from '../utils';
-//storage.remove("token")
+let token: string = storage.get("token");
+if (!token) {
+    HandleTokenError();
+}
+axios({
+    method: "GET",
+    url: "/login/users/me",
+    headers: {
+        authorization: `Bearer ${token}`
+    }
+}).then(response => {
+}).catch(error => {
+    switch (error.message) {
+        case "Request failed with status code 404":
+        case "Network Error":
+            message.error("网络错误。");
+            break;
+        case "Request failed with status code 401":
+            switch (error.response.data.detail) {
+                case "Could not validate credentials":
+                    storage.remove("token");
+                    HandleTokenError();
+                    break;
+            }
+            break;
+        default:
+            message.error("网络错误。");
+            break;
+    }
+})
 </script>
