617671d82f6685892e5291d682ea5f6c86c42507
殊怀丶 <lvzhihe_123@qq.com>
2021-10-18 14:26:51 +0800

    feat: 在顶栏上增加退出按钮

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -1,33 +1,46 @@
 <template>
     <n-layout-header bordered>
         <n-menu mode="horizontal" :inverted="inverted" :options="menuOptions" />
+        <n-button class="exitButton" :bordered="false" v-on:click="showExitModal = true">
+            <n-icon size="30">
+                <exit-outline />
+            </n-icon>
+        </n-button>
     </n-layout-header>
+    <n-modal v-model:show="showExitModal">
+        <n-card class="exitModalCard" title="提示" :bordered="false" size="huge">
+            <div class="exitModalContent">即将退出登录，是否继续？</div>
+            <n-button class="confirmButton" v-on:click="ExitLogin">确认</n-button>
+            <n-button class="returnButton" type="primary" v-on:click="showExitModal = false">返回</n-button>
+        </n-card>
+    </n-modal>
 </template>
-
 <script lang="ts" setup>
-import { defineComponent, h, ref } from 'vue'
-import {
-    BookOutline as BookIcon,
-} from '@vicons/ionicons5'
+import { defineComponent, h, ref } from 'vue';
+import { Ref } from "@vue/reactivity";
 import { NIcon } from 'naive-ui';
-
-
-const inverted = ref(false)
-
-function renderIcon(icon: any) {
-    return () => h(NIcon, null, { default: () => h(icon) })
-}
-
-const menuOptions = [
+import { BookOutline as BookIcon, ExitOutline, Exit } from '@vicons/ionicons5';
+import { Router, useRouter } from "vue-router";
+import { storage } from '../utils';
+let inverted: Ref<boolean> = ref(false);
+let showExitModal: Ref<boolean> = ref(false);
+let router: Router = useRouter();
+defineComponent({
+    components: {
+        ExitOutline,
+        Exit
+    }
+});
+let menuOptions: Array<object> = [
     {
         label: '且听风吟',
         key: 'hear-the-wind-sing',
-        icon: renderIcon(BookIcon)
+        icon: RenderIcon(BookIcon)
     },
     {
         label: '1973年的弹珠玩具',
         key: 'pinball-1973',
-        icon: renderIcon(BookIcon),
+        icon: RenderIcon(BookIcon),
         disabled: true,
         children: [
             {
@@ -40,10 +53,35 @@ const menuOptions = [
         label: '寻羊冒险记',
         key: 'a-wild-sheep-chase',
         disabled: true,
-        icon: renderIcon(BookIcon)
+        icon: RenderIcon(BookIcon)
     },
-]
-
-
+];
+function ExitLogin(): void {
+    storage.remove("token");
+    storage.remove("saveID");
+    router.push({ name: "login" });
+}
+function RenderIcon(icon: any) {
+    return () => h(NIcon, null, { default: () => h(icon) });
+}
 </script>
-
+<style>
+.exitButton {
+    position: absolute;
+    right: 10px;
+    margin-top: 5px;
+}
+.exitModalCard {
+    width: 300px;
+    text-align: center;
+}
+.exitModalContent {
+    font-size: medium;
+}
+.confirmButton {
+    margin-top: 15px;
+}
+.returnButton {
+    margin-left: 15px;
+}
+</style>
