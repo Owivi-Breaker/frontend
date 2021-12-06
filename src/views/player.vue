33ba67349fc994056f15899e543096e9574b7978
MelonSoul <1375911011@qq.com>
2021-12-06 17:07:47 +0800

    build: 球员数据过滤功能尝试

--- src/views/player.vue
+++ src/views/player.vue
@@ -1,38 +1,52 @@
 <template>
-    <va-data-table :items="v" v-show="showTable">
-        <template #cell(age)="{ source: age }">
-            <va-chip>{{ age }}</va-chip>
-        </template>
+    <va-data-table :items="PlayerData" v-show="showTable">
+        <template #header(translated_name)>姓名</template>
+        <template #header(translated_nationality)>国籍</template>
+        <template #cell()="{ source: age }">{{ age + 100 }}</template>
     </va-data-table>
-
-    {{ v }}
 </template>
 
 <script lang="ts" setup>
 import { getClubPlayerAPI } from '@/api/player';
-import { onMounted, reactive, ref } from 'vue'
+import { onMounted, reactive, ref, computed } from 'vue'
 
-let v = ref([])
+
+/* 获取球员数据 */
+let rawPlayerData = ref([])
 let showTable = ref(false)
-
-// interface PlayerConfig {
-//     translated_name: string;
-//     translated_nationality: string;
-//     age: number;
-//     shooting: number;
-//     passing: number;
-//     dribbling: number;
-//     interception: number;
-//     pace: number;
-// }
-
 onMounted(
     () => {
         getClubPlayerAPI({ club_id: 1 })
             .then((res) => {
-                v.value = res
+                rawPlayerData.value = res
                 showTable.value = true
             })
     }
 )
+
+/* 处理球员数据 */
+const PlayerData = computed(() =>
+    rawPlayerData.value.map(handleRawPlayerData)
+)
+
+const handleRawPlayerData = (value: any) => {
+    let a: any = {}
+
+    a['姓名'] = value.translated_name
+    a['年龄'] = value.age
+    a['国籍'] = value.translated_nationality
+    // a['出生日期'] = value.birth_date
+    a['射门'] = parseInt(value.shooting)
+    a['过人'] = parseInt(value.dribbling)
+    a['防守'] = parseInt(value.interception)
+    a['速度'] = parseInt(value.pace)
+    a['体力'] = parseInt(value.stamina)
+    a['守门'] = parseInt(value.goalkeeping)
+    a['侵略'] = parseInt(value.aggression)
+    a['任意球'] = parseInt(value.free_kick)
+
+    return a
+}
+
+
 </script>
