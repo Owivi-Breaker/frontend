378460272ababe64d2aeb79e342286af277de448
MelonSoul <1375911011@qq.com>
2022-01-13 16:28:28 +0800

    Changes

--- src/components/NextGameHome.vue
+++ src/components/NextGameHome.vue
@@ -1,11 +1,9 @@
 <template>
     <n-card class="nextGameCard" title="下一场比赛-未开始">
-        <n-grid x-gap="12" :cols="21">
+        <n-grid :cols="21" x-gap="12">
             <n-gi span="10">
                 <div class="firstTeam">
-                    <svg class="avatar">
-                        <Avataaars />
-                    </svg>
+                    <img :src="'http://s1.s100.vip:13127/Public/'+teams[0]+'.png'" alt="图片" class="avatar">
                     <span class="tenSpan"></span>
                     <span class="name">{{ teams[0] }}</span>
                     <span class="fiveSpan"></span>
@@ -21,9 +19,7 @@
                     <span class="tenSpan"></span>
                     <span class="name">{{ teams[1] }}</span>
                     <span class="fiveSpan"></span>
-                    <svg class="avatar">
-                        <Avataaars />
-                    </svg>
+                    <img :src="'http://s1.s100.vip:13127/Public/'+teams[1]+'.png'" alt="图片" class="avatar">
                 </div>
             </n-gi>
         </n-grid>
@@ -33,10 +29,10 @@
     </n-card>
 </template>
 <script lang="ts" setup>
-import Avataaars from 'vuejs-avataaars/src/Avataaars.vue'
 import { ref, Ref } from "vue";
 import { getIncomingGamesAPI } from "@/apis/club";
 import { getDateAPI } from '@/apis/user';
+
 let teams: Ref<Array<string>> = ref(["", ""]);
 let nextGameName: Ref<string> = ref("");
 let nextGameDate: Ref<string> = ref("");
@@ -49,45 +45,56 @@ getIncomingGamesAPI().then(response => {
     getDateAPI().then(response => {
         let curDate: number = (new Date(response.date)).getTime() / 1000;
         distance.value = (new Date(nextGameDate.value).getTime() / 1000 - curDate) / 24 / 60 / 60;
-    }).catch((_error: {}) => { });
-}).catch((_error: {}) => { });
+    }).catch((_error: {}) => {
+    });
+}).catch((_error: {}) => {
+});
 </script>
 <style>
 .nextGameCard {
     margin-bottom: 16px;
 }
+
 .firstTeam {
     display: flex;
     justify-content: end;
     align-items: Center;
 }
+
 .secondTeam {
     display: flex;
     justify-content: start;
     align-items: Center;
 }
+
 .name {
     font-size: xx-large;
 }
+
 .point {
     font-size: 50px;
 }
+
 .gameName,
 .date,
 .fromNow {
     text-align: center;
     font-size: large;
 }
+
 .colon {
     font-size: 50px;
     text-align: center;
 }
+
 .tenSpan {
     width: 10%;
 }
+
 .fiveSpan {
     width: 5%;
 }
+
 .avatar {
     width: 80px;
     height: 80px;
