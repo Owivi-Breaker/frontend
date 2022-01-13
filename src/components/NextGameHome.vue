1462b05a4f2fb220fa744d22fbd40abf69f8d8e8
殊怀丶 <lvzhihe_123@qq.com>
2022-01-13 18:04:00 +0800

    fix: 优化图片不显示的问题

--- src/components/NextGameHome.vue
+++ src/components/NextGameHome.vue
@@ -1,38 +1,40 @@
 <template>
     <n-card class="nextGameCard" title="下一场比赛-未开始">
-        <n-grid :cols="21" x-gap="12">
-            <n-gi span="10">
-                <div class="firstTeam">
-                    <img :src="'http://s1.s100.vip:13127/Public/'+teams[0]+'.png'" alt="图片" class="avatar">
-                    <span class="tenSpan"></span>
-                    <span class="name">{{ teams[0] }}</span>
-                    <span class="fiveSpan"></span>
-                    <span class="point">0</span>
-                </div>
-            </n-gi>
-            <n-gi span="1">
-                <div class="colon">:</div>
-            </n-gi>
-            <n-gi span="10">
-                <div class="secondTeam">
-                    <span class="point">0</span>
-                    <span class="tenSpan"></span>
-                    <span class="name">{{ teams[1] }}</span>
-                    <span class="fiveSpan"></span>
-                    <img :src="'http://s1.s100.vip:13127/Public/'+teams[1]+'.png'" alt="图片" class="avatar">
-                </div>
-            </n-gi>
-        </n-grid>
-        <div class="gameName">{{ nextGameName }}</div>
-        <div class="date">比赛时间&nbsp;{{ nextGameDate }}</div>
-        <div class="fromNow">距今还有&nbsp;{{ distance }}&nbsp;天</div>
+        <div v-if="!isLoading">
+            <n-grid :cols="21" x-gap="12">
+                <n-gi span="10">
+                    <div class="firstTeam">
+                        <img :src="'http://s1.s100.vip:13127/Public/' + teams[0] + '.png'" alt="图片" class="avatar" />
+                        <span class="tenSpan"></span>
+                        <span class="name">{{ teams[0] }}</span>
+                        <span class="fiveSpan"></span>
+                        <span class="point">0</span>
+                    </div>
+                </n-gi>
+                <n-gi span="1">
+                    <div class="colon">:</div>
+                </n-gi>
+                <n-gi span="10">
+                    <div class="secondTeam">
+                        <span class="point">0</span>
+                        <span class="tenSpan"></span>
+                        <span class="name">{{ teams[1] }}</span>
+                        <span class="fiveSpan"></span>
+                        <img :src="'http://s1.s100.vip:13127/Public/' + teams[1] + '.png'" alt="图片" class="avatar" />
+                    </div>
+                </n-gi>
+            </n-grid>
+            <div class="gameName">{{ nextGameName }}</div>
+            <div class="date">比赛时间&nbsp;{{ nextGameDate }}</div>
+            <div class="fromNow">距今还有&nbsp;{{ distance }}&nbsp;天</div>
+        </div>
     </n-card>
 </template>
 <script lang="ts" setup>
 import { ref, Ref } from "vue";
 import { getIncomingGamesAPI } from "@/apis/club";
 import { getDateAPI } from '@/apis/user';
-
+let isLoading: Ref<boolean> = ref(true);
 let teams: Ref<Array<string>> = ref(["", ""]);
 let nextGameName: Ref<string> = ref("");
 let nextGameDate: Ref<string> = ref("");
@@ -45,10 +47,9 @@ getIncomingGamesAPI().then(response => {
     getDateAPI().then(response => {
         let curDate: number = (new Date(response.date)).getTime() / 1000;
         distance.value = (new Date(nextGameDate.value).getTime() / 1000 - curDate) / 24 / 60 / 60;
-    }).catch((_error: {}) => {
-    });
-}).catch((_error: {}) => {
-});
+        isLoading.value = false;
+    }).catch((_error: {}) => { });
+}).catch((_error: {}) => { });
 </script>
 <style>
 .nextGameCard {
