c753ee4f14f7d1bef64fcb5b05fef61aabc0e8e3
MelonSoul <1375911011@qq.com>
2022-02-03 11:40:34 +0800

    fix(src/main.ts): 解决了 tailwind 可能会覆盖 naiveui 组件样式的问题

--- OwiviOsa前端/src/main.ts
+++ OwiviOsa前端/src/main.ts
@@ -3,9 +3,14 @@ import App from './App.vue'
 import router from '@/router' // 路由引入
 import naive from './naive' // naive-ui 组件局部引入
 import { createPinia } from 'pinia' // pinia引入
-const app = createApp(App)
-import './index.css'
+import './index.css' // tailwind引入
 
+// 避免tailwind对naiveui的样式覆盖
+const meta = document.createElement('meta')
+meta.name = 'naive-ui-style'
+document.head.appendChild(meta)
+
+const app = createApp(App)
 
 app.use(router)
 app.use(naive)
